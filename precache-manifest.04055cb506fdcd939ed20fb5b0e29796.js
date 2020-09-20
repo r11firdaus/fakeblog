b9e299e8fbb76113ef0bf40721c357bcad05f726
r11firdaus <66978415+r11firdaus@users.noreply.github.com>
2020-09-20 07:52:43 +0700

    Add files via upload

---
+++ precache-manifest.04055cb506fdcd939ed20fb5b0e29796.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b1a045d83c9ad839d517e2510aad26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a8b9e3bf607e3511ad2",
+    "url": "./static/css/main.ca41b624.chunk.css"
+  },
+  {
+    "revision": "b4672a8a3bf40d2d4e82",
+    "url": "./static/js/2.9b58b314.chunk.js"
+  },
+  {
+    "revision": "2a8b9e3bf607e3511ad2",
+    "url": "./static/js/main.429bb497.chunk.js"
+  },
+  {
+    "revision": "d60e92bfddf2b702e095",
+    "url": "./static/js/runtime-main.fb57f07a.js"
+  }
+]);
